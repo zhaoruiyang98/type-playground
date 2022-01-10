eee94d122e319bb891bdacb95b93859d10cdb673
zhaoruiyang98 <zhaoruiyang19@mails.ucas.edu.cn>
2022-01-10 14:17:52 +0800

    add(protocol_name): example of __x in protocol

---
+++ src/protocol_name.py
@@ -0,0 +1,25 @@
+from typing import (
+    Protocol,
+)
+
+
+class Readable(Protocol):
+    def read(self, __x): ...
+
+
+class Test:
+    def read(self, x):
+        pass
+
+
+class Test2:
+    def read(self, t):
+        pass
+
+
+def foo(x: Readable):
+    x.read(x=3) # error
+
+
+foo(Test())
+foo(Test2())
